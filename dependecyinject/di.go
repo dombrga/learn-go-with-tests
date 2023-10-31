3b2bee402d410b7d5c0b4bfa27cd5b76fd2236fc
modb <dominic.o.barga@accenture.com>
2023-11-01 02:04:49 +0800

    dependency injection

---
+++ dependecyinject/di.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+// func Greet(writer *bytes.Buffer, name string) {
+// 	writer.Write([]byte(fmt.Sprintf("Hello, %s", name)))
+// }
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
